8577650dc7d123c3e6b00937cc177e105434018f
JSD  ~ [ SafeGuard AI ] <123333887+JSDWRLD@users.noreply.github.com>
2024-09-12 21:33:13 -0700

    Create SimpleLoading.kt

---
+++ SimpleLoading.kt
@@ -0,0 +1,22 @@
+@Composable
+fun SimpleLoading(message: String) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier.size(64.dp),
+                strokeWidth = 6.dp
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            Text(
+                text = message,
+                fontSize = 24.sp,
+            )
+        }
+    }
+}
